59f1281132e0af8206a7ee049b0d2ae9978d7d5c
21x37 <tylermok@outlook.com>
2019-05-06 19:56:16 -0700

    added projects

--- src/components/projects/IndividualProject.js
+++ src/components/projects/IndividualProject.js
@@ -7,7 +7,21 @@ class IndividualProject extends React.Component {
     render() {
         return (
             <div>
-                Project
+                {this.props.projects.map((project) => {
+                    return (
+                        <div>
+                            <h2>{project.name}</h2>
+                            <p>{project.description}</p>
+                            {project.technologies.map((item) => {
+                                return (
+                                    <p>{item}</p>
+                                )
+                            })}
+                            <a href={project.github} target='_blank'>Github Repo</a>
+                            {project.website && <a href={project.website} target='_blank'>Website</a>}
+                        </div>
+                    )
+                })}
             </div>
         );
     };
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,20 +6,18 @@ class Projects extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            projects: ['1', '2']
+            projects: [
+                { name: 'Training Pals', description: 'A social media to track your calories, and connect with friends!', technologies: ['React', 'Redux', 'Firebase', 'Node', 'Sass'], github: 'https://github.com/21x37/calorie-tracker', website: 'https://trainingpals.herokuapp.com' },
+                { name: 'Freight Inc', description: 'A freight broker website to make dispatching trucks easier!', technologies: ['React', 'Node', 'Sass',], github: 'https://github.com/21x37/freight-con', website: 'http://freight-website.herokuapp.com/' },
+                { name: 'comment-bot', description: 'npm module to easily setup a reddit comment bot!', technologies: ['Node'], github: 'https://github.com/21x37/comment-bot', npm: 'https://www.npmjs.com/package/comment-bot' }
+            ]
         }
     }
     render() {
         return (
             <div>
                 <Header />
-                {this.state.projects.map(() => {
-                    return (
-                        <div>
-                            <IndividualProject />
-                        </div>
-                    )
-                })}
+                <IndividualProject projects={this.state.projects} />
                 Project Page
             </div>
         );
